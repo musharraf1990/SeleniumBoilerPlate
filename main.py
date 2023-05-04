909e29ee91088df966388895a43b70ccb2351146
Anees Mufti <89216202+MuftiAnees@users.noreply.github.com>
2023-05-04 09:48:37 +0500

    Main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from time import sleep
+from colorama import Fore, Back, Style
+from selenium.common.exceptions import NoSuchElementException
+
+PATH = "C:\\Users\\mufti\\OneDrive\\Documents\\Github\\Selenium\\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+driver.maximize_window()
+driver.get("http://172.16.10.4:8082/jw/web/login")
+sleep(2)
+search = driver.find_element(By.ID, "j_username")
+search.send_keys("EnterUserNameHere")
+search = driver.find_element(By.ID, "j_password")
+search.send_keys("EnterPasswordHere")
+search = driver.find_element(By.NAME, "submit").click()
+print("Login Successful!")
+sleep(2)
+
+os.system('cls')
+print(Fore.GREEN+'------------------------------------------------------------Test Started!------------------------------------------------------------')
+print(Fore.RESET)
