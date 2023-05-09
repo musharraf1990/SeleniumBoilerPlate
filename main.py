ddab86386454983576e115ee5f84e7b8ff2020f3
Anees Mufti <89216202+MuftiAnees@users.noreply.github.com>
2023-05-09 09:25:57 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from time import sleep
-from colorama import Fore, Back, Style
 from selenium.common.exceptions import NoSuchElementException
 
 PATH = "C:\\Users\\mufti\\OneDrive\\Documents\\Github\\Selenium\\chromedriver.exe"
@@ -20,5 +19,3 @@ print("Login Successful!")
 sleep(2)
 
 os.system('cls')
-print(Fore.GREEN+'------------------------------------------------------------Test Started!------------------------------------------------------------')
-print(Fore.RESET)
